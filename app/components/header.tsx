7c83d6757c751c736d77917c5de47da8e8ab79a2
saikumar05g2 <78261043+saikumar05g2@users.noreply.github.com>
2024-05-17 16:35:07 +0530

    Fixed mobile header (#1)
    Header Refinement

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,18 +1,28 @@
+'use client'
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
-const header = () => {
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="header">
-      <h1>Food</h1>
-      <div className="right-header">
-        <Link href="/register">Register</Link> {" "}
-        <Link href="/about">About</Link>{" "}
+      <Link href="/" className="header-name">Food</Link>
+      <div className={`right-header ${isOpen ? "open" : ""}`}>
+        <Link href="/register">Register</Link>
+        <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        {isOpen ? <div className="close-icon">✖</div> : "☰"}
       </div>
     </div>
   );
 };
 
-export default header;
+export default Header;
